aed7c0fe6c196d6d6ac6214687f4d7d63bb8e70a
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-18 10:47:00 -0800

    expand logic to include all combinations, confirm correct score and display branching messages via alerts

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,20 @@ $(document).ready(function() {
 
     var score = answer1 + answer2
 
-    if (score >= 4) {
-      alert(score);
-      $(".results").show();
+    if (score >= 2 && score <=3) {
+      alert("Your score is: " + score + "." + "Try track 1!");
+      // $(".results").show();
     }
+
+    if (score > 3 && score <=4) {
+      alert("Your score is: " + score + "." + "Try track 2!");
+    }
+
+    if (score >= 5) {
+      alert("Your score is: " + score + "." + "Try track 3!");
+    }
+
+
 
   });
 });
