34bdecb1cf6cce7ec2dad49db3b357a7be15fb6a
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-18 10:38:46 -0800

    create new variable score in scripts, and confirm basic logic is working

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,13 @@ $(document).ready(function() {
     var answer1 = parseInt($("input:radio[name=answer1]:checked").val());
     console.log(answer1);
 
-    if (answer1 >= 1) {
-      alert(answer1);
+    var answer2 = parseInt($("input:radio[name=answer2]:checked").val());
+    console.log(answer2);
+
+    var score = answer1 + answer2
+
+    if (score >= 4) {
+      alert(score);
       $(".results").show();
     }
 
