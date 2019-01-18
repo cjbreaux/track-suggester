418f7fe9913764247101e04b1970ab8df0df85cc
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-18 10:11:36 -0800

    give different names to each set of radio buttons each group may be selected, remove questions div, test branching in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,12 @@ $(document).ready(function() {
   $("form#quiz").submit(function(event) {
     event.preventDefault();
 
-    var answer1 = parseInt($("input:radio[name=answer]:checked").val());
+    var answer1 = parseInt($("input:radio[name=answer1]:checked").val());
     console.log(answer1);
+
+    if (answer1 >= 1) {
+      alert(answer1);
+    }
+
   });
 });
