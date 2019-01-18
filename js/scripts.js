6c33f1a3a61acf0cde2001b6d975cb214deb67ff
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-18 12:00:05 -0800

    alert an error msg if user leaves a question unanswered

--- js/scripts.js
+++ js/scripts.js
@@ -9,33 +9,37 @@ $(document).ready(function() {
     console.log(answer2);
 
     var answer3 = parseInt($("input:radio[name=answer3]:checked").val());
-    console.log(answer2);
+    console.log(answer3);
 
     var answer4 = parseInt($("input:radio[name=answer4]:checked").val());
-    console.log(answer2);
+    console.log(answer4);
 
     var answer5 = parseInt($("input:radio[name=answer5]:checked").val());
-    console.log(answer2);
+    console.log(answer5);
 
     var score = answer1 + answer2 + answer3 + answer4 + answer5
 
     $(".results p, .greeting").addClass("hidden");
 
-    if (score >= 5 && score <=7) {
-      alert("Your score is: " + score + "." + "Try track: Front End!");
-      $("#ruby").removeClass("hidden");
-    }
+    // this quiz is weighted more heavily towrds a generalist track
+    if (score){
+      if (score >= 5 && score <=7) {
+        alert("Your score is: " + score + "." + "Try track: Front End!");
+        $("#ruby").removeClass("hidden");
+      }
 
-    if (score > 7 && score <=12) {
-      alert("Your score is: " + score + "." + "Try track: General!");
-      $("#PHP").removeClass("hidden");
-    }
+      if (score > 7 && score <=12) {
+        alert("Your score is: " + score + "." + "Try track: General!");
+        $("#PHP").removeClass("hidden");
+      }
 
-    if (score >= 13) {
-      alert("Your score is: " + score + "." + "Try track: Back End!");
-      $("#Java").removeClass("hidden");
+      if (score >= 13) {
+        alert("Your score is: " + score + "." + "Try track: Back End!");
+        $("#Java").removeClass("hidden");
+      }
+    } else {
+        alert("Please answer all questions before submitting.");
     }
-
 
 
   });
