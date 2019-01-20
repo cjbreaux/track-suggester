9fb198a992a7d713bcf90b38dd3abdb868094999
Chris Breaux <christopherjbreaux@gmail.com>
2019-01-20 15:05:48 -0800

    change submit button color, size and text as well as justify all the cards to the center

--- js/scripts.js
+++ js/scripts.js
@@ -33,25 +33,17 @@ $(document).ready(function() {
     event.preventDefault();
 
     var answer1 = parseInt($("input:radio[name=answer1]:checked").val());
-    console.log(answer1);
-
     var answer2 = parseInt($("input:radio[name=answer2]:checked").val());
-    console.log(answer2);
-
     var answer3 = parseInt($("input:radio[name=answer3]:checked").val());
-    console.log(answer3);
-
     var answer4 = parseInt($("input:radio[name=answer4]:checked").val());
-    console.log(answer4);
-
     var answer5 = parseInt($("input:radio[name=answer5]:checked").val());
-    console.log(answer5);
 
     var score = answer1 + answer2 + answer3 + answer4 + answer5
 
-    $(".results p, .greeting").addClass("hidden");
+    // $(".results p, .greeting").addClass("hidden");
+    $(".results").children().addClass("hidden");
 
-    // this quiz is weighted more heavily towrds an android track
+    // this quiz is weighted more heavily towards an android track
     if (score){
       if (score >= 5 && score <=7) {
         $("#frontEnd").toggleClass("hidden");
